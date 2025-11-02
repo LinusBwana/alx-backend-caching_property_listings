0d0ab27597fa728197c16cbf48ef2d89b2e7d635
LinusBwana <linusbwana49@gmail.com>
2025-11-02 23:12:02 +0300

    Changed from classbased views to function based views

--- properties/views.py
+++ properties/views.py
@@ -1,17 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from django.utils.decorators import method_decorator
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
 # Create your views here.
-@method_decorator(cache_page(60 * 15), name='dispatch')
-class PropertyListView(ListView):
-    model = Property
-    template_name = 'properties/property_list.html'
-    context_object_name = 'properties'
-
-    def get_queryset(self):
-        """Return all properties."""
-        return Property.objects.all()
-
+@cache_page(60 * 15)
+def property_list(request):
+    """Return a cached JSON response of all properties."""
+    properties = Property.objects.all().values(
+        'id', 'title', 'description', 'price', 'location', 'created_at'
+    )
+    data = list(properties)
+    return JsonResponse({'data': data})
