b4e455982169a0a26075601567f0857a176fe935
LinusBwana <linusbwana49@gmail.com>
2025-11-02 23:08:16 +0300

    Created views and configured the URLs

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from .models import Property
 
 # Create your views here.
+@method_decorator(cache_page(60 * 15), name='dispatch')
+class PropertyListView(ListView):
+    model = Property
+    template_name = 'properties/property_list.html'
+    context_object_name = 'properties'
+
+    def get_queryset(self):
+        """Return all properties."""
+        return Property.objects.all()
+
